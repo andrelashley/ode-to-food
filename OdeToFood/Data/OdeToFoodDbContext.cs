ef4b304ad53bbcc0213fac7ce30bf197c3c5aa01
André Lashley <andre.lashley@gmail.com>
2018-05-06 23:37:51 -0700

    Implement a DbContext

---
+++ OdeToFood/Data/OdeToFoodDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Models;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+
+        public OdeToFoodDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
