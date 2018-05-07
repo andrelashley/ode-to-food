d12ee8cafbc113fe1ff9d687373e19869aeca6b8
André Lashley <andre.lashley@gmail.com>
2018-05-06 19:15:37 -0700

    Initial commit

---
+++ OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    [Route("company/[controller]/[action]")]
+    public class AboutController
+    {
+        [Route("")]
+        public string Phone()
+        {
+            return "1-250-813-0854";
+        }
+
+        public string Address()
+        {
+            return "Canada";
+        }
+    }
+}
