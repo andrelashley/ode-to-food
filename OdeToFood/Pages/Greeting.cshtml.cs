a970eda8740c5aa2803282748a5efb9161fd3812
André Lashley <andre.lashley@gmail.com>
2018-05-14 23:16:57 -0700

    Implement a greeting razor page

---
+++ OdeToFood/Pages/Greeting.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Services;
+
+namespace OdeToFood.Pages
+{
+    public class GreetingModel : PageModel
+    {
+        private IGreeter _greeter;
+
+        public string CurrentGreeting { get; set; }
+
+        public GreetingModel(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public void OnGet()
+        {
+            CurrentGreeting = _greeter.GetMessageOfTheDay();
+        }
+    }
+}
